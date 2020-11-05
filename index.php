e1929622236f0cbb5c11d551ba5cca1191abcbbc
DivinePoseidon <piskovyi.dmytro@gmail.com>
2020-11-05 13:59:48 +0200

    finish task

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
-<?php declare(strict_types=1);
+<?php
+declare(strict_types = 1);
+
+use app\PhotoProcessor;
+use app\Rekognition;
+use app\storage\S3Storage;
+use app\UploadedPhoto;
+use Aws\S3\S3Client;
 
 require __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->required(['AWS_KEY', 'AWS_SECRET']);
+$dotenv->load();
 
-return function ($event) {
-    return 'Hello ' . ($event['name'] ?? 'world');
+
+return static function (array $event) {
+    $uploadedPhotos = UploadedPhoto::createObjectsFromEvent($event);
+    $uploadedPhotosLabels = (new Rekognition($uploadedPhotos))->detectLabelsFromUploadedPhotos();
+    (new PhotoProcessor($uploadedPhotosLabels))->processLabels();
 };
+
